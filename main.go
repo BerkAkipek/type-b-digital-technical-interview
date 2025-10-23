30930f226b7715d8894177937e3e02432b4a65e4
BerkAkipek <berk.akipek.96@gmail.com>
2025-10-23 18:46:32 +0300

    optimized with redundant if statement removed

--- main.go
+++ main.go
@@ -2,17 +2,13 @@ package main
 
 import "fmt"
 
-// FibSum returns the sum of all even integers in given interval
+// FibSum returns the sum of all even Fibonacci numbers up to n
 func FibSum(n int) int {
-	if n <= 1 {
-		return 0
-	}
-
 	it1, it2 := 0, 1
 	total := 0
 	for it1 <= n {
 		if it1&1 == 0 {
-			total = total + it1
+			total += it1
 		}
 		it1, it2 = it2, it1+it2
 	}
@@ -20,6 +16,8 @@ func FibSum(n int) int {
 }
 
 func main() {
-	fmt.Println(FibSum(2))
-	fmt.Println(FibSum(8))
+	cases := []int{-2, -1, 0, 1, 2, 8, 10, 34, 100}
+	for _, n := range cases {
+		fmt.Printf("FibSum(%d) = %d\n", n, FibSum(n))
+	}
 }
--- main_test.go
+++ main_test.go
@@ -8,16 +8,16 @@ func TestFibSum(t *testing.T) {
 		input    int
 		expected int
 	}{
-		{"n is -2", -2, 0},
-		{"n is 0", 0, 0},
-		{"n is 1", 1, 0},
-		{"n is 2", 2, 2},
-		{"n is 3", 3, 2},
-		{"n is 8", 8, 10},
-		{"n is 10", 10, 10},
-		{"n is 34", 34, 44},
-		{"n is 100", 100, 44},
-		{"n is 1000", 1000, 798},
+		{"input is -2", -2, 0},
+		{"input is 0", 0, 0},
+		{"input is 1", 1, 0},
+		{"input is 2", 2, 2},
+		{"input is 3", 3, 2},
+		{"input is 8", 8, 10},
+		{"input is 10", 10, 10},
+		{"input is 34", 34, 44},
+		{"input is 100", 100, 44},
+		{"input is 1000", 1000, 798},
 	}
 
 	for _, testCase := range tests {
